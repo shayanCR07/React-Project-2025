59f6852a4101817e42dd1511b1490e0350d1243f
shayanCR07 <shayan1985@gmail.com>
2025-07-17 11:12:48 +0530

    contact and footer done

--- react_country/src/components/UI/Footer.jsx
+++ react_country/src/components/UI/Footer.jsx
@@ -1,3 +1,32 @@
+import footerContact from "../../api/footerApi.json";
+import { IoCallSharp } from "react-icons/io5";
+import { MdPlace } from "react-icons/md";
+import { TbMailPlus } from "react-icons/tb";
+
 export const Footer = () => {
-    return <h1>Hello Footer</h1>
-}
+
+    const footerIcon = {
+        MdPlace : <MdPlace />,
+        IoCallSharp : <IoCallSharp/>,
+        TbMailPlus : <TbMailPlus />
+    }
+  return (
+    <footer className="footer-section">
+      <div className="container grid grid-three-cols">
+        {footerContact.map((currData, index) => {
+          const { icon, title, details } = currData;
+        
+        return (
+            <footer className="footer-contact" key={index}>
+            <div className="icon">{footerIcon[icon]}</div>
+            <div className="footer-contact-text">
+                <p>{title}</p>
+                <p>{details}</p>
+            </div>
+            </footer>
+        );
+        })}
+      </div>
+    </footer>
+  );
+};
--- react_country/src/pages/Contact.jsx
+++ react_country/src/pages/Contact.jsx
@@ -1,3 +1,43 @@
 export const Contact = () => {
-    return <h1>Contact Page</h1>
-}
+    const handleFormSubmit = (formData) => {
+        // console.log(formData.entries());
+        const formInputData = Object.fromEntries(formData.entries());
+        console.log(formInputData);
+        
+        
+    };
+  return (
+    <section className="section-contact">
+      <h2 className="container-title">Contact Us</h2>
+    <div className="contact-wrapper container">
+      <form action={handleFormSubmit}>
+        <input
+          type="text"
+          className="form-control"
+          required
+          autoComplete="false"
+          placeholder="Enter Your Name"
+          name="username"
+        />
+        <input
+          type="email"
+          className="form-control"
+          required
+          autoComplete="false"
+          placeholder="Enter Your Email"
+          name="email"
+        />
+        <textarea
+          className="form-control"
+          rows="10"
+          placeholder="Enter your Text"
+          name="message"
+          required
+          autoComplete="false"
+        ></textarea>
+        <button type="submit" value="send">Send</button>
+      </form>
+    </div>
+    </section>
+  );
+};
